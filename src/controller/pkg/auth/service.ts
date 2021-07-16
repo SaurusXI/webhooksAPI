930c903f93028587a7a6fb1366acc1d2366de8e3
SaurusXI <vermashantanu@hotmail.com>
2021-07-16 15:09:32 +0530

    feat(api): add auth service

---
+++ src/controller/pkg/auth/service.ts
@@ -0,0 +1,27 @@
+import { v4 as uuidv4 } from 'uuid';
+
+class AuthService {
+  // Admin ID changes every time server is restarted, so token will keep changing
+  adminId: string
+
+  constructor() {
+    this.adminId = uuidv4();
+  }
+
+  authenticate(username: string, password: string) {
+    if (username === process.env.USER && password === process.env.PASS) {
+      return this.adminId;
+    }
+
+    return null;
+  }
+
+  validate(uid: string) {
+    if (uid === this.adminId) {
+      return true;
+    }
+    return false;
+  }
+}
+
+export { AuthService as default };
