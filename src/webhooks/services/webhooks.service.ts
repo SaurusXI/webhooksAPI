1c7f6157620e304041e6c8d387a35123a0ca6f2b
SaurusXI <vermashantanu@hotmail.com>
2021-07-17 02:24:42 +0530

    fix(webhooks): incorrect RPC call signature

--- src/webhooks/services/webhooks.service.ts
+++ src/webhooks/services/webhooks.service.ts
@@ -1,15 +1,16 @@
 /* eslint-disable import/extensions */
+import { Context } from 'moleculer';
 import modelsvc from '../pkg/model/service';
 
 const webhooksMicroservice = {
   name: 'webhooks',
   actions: {
-    async register(targetUrl: string) {
-      return modelsvc.registerURL(targetUrl);
+    async register(ctx: Context<{targetUrl: string}, {}>) {
+      return modelsvc.registerURL(ctx.params.targetUrl);
     },
 
-    async update(id: string, newTargetUrl: string) {
-      return modelsvc.updateURLForID(id, newTargetUrl);
+    async update(ctx: Context<{id: string, newTargetUrl: string}, {}>) {
+      return modelsvc.updateURLForID(ctx.params.id, ctx.params.newTargetUrl);
     },
 
     async list() {
