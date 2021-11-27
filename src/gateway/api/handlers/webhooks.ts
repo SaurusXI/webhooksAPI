d71db87efad2151c22423d19290370c50e2e76c1
Shantanu Verma <vermashantanu@hotmail.com>
2021-11-28 00:46:45 +0530

    fix: use appropriate http verbs in api structure

--- src/gateway/api/handlers/webhooks.ts
+++ src/gateway/api/handlers/webhooks.ts
@@ -11,7 +11,7 @@ const fetch = (rpcsvc: RPCService) => async (req: Request, res: Response) => {
 };
 
 const register = (rpcsvc: RPCService) => async (req: Request, res: Response) => {
-  const url = req.query.targetUrl;
+  const url = req.body.targetUrl;
   if (typeof url !== 'string') {
     res.status(400).json({
       msg: 'Malformed request',
@@ -26,8 +26,8 @@ const register = (rpcsvc: RPCService) => async (req: Request, res: Response) => 
 };
 
 const update = (rpcsvc: RPCService) => async (req: Request, res: Response) => {
-  const newUrl = req.query.newTargetUrl;
-  const urlId = req.query.id;
+  const newUrl = req.body.newTargetUrl;
+  const urlId = req.body.id;
 
   if (typeof newUrl !== 'string' || typeof urlId !== 'string') {
     res.status(400).json({
@@ -58,8 +58,8 @@ const trigger = (rpcsvc: RPCService) => async (req: Request, res: Response) => {
 
 const registerHandlers = async (app: Express, JWT: authJWT, rpcsvc: RPCService) => {
   app.get('/list', JWT.authenticate(), fetch(rpcsvc));
-  app.get('/update', JWT.authenticate(), update(rpcsvc));
-  app.get('/register', JWT.authenticate(), register(rpcsvc));
+  app.patch('/webhook', JWT.authenticate(), update(rpcsvc));
+  app.post('/webhook', JWT.authenticate(), register(rpcsvc));
   app.get('/trigger', JWT.authenticate(), trigger(rpcsvc));
 };
 
